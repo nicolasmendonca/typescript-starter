02e68b305121ae605778ee9fd287566ece2734bf
Nicolas Mendonca <ae_nmendonca@idcontractors.net>
2020-02-10 17:14:44 -0300

    Inital commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "printWidth": 80,
+  "tabWidth": 2,
+  "semi": true,
+  "bracketSpacing": true,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "arrowParens": "avoid",
+  "useTabs": true
+}
